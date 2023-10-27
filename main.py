96a386d722eb6512f0c27ddf0cb959cabae7b1cd
Sourasish Basu <89185962+SourasishBasu@users.noreply.github.com>
2023-10-27 16:53:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console and set the environment variables in your local environment.
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
+account_sid = 'TWILIO_ACCOUNT_SID'
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
