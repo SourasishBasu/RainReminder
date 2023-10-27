ed308a399f2b93ca11707de64603e0d0a07084b9
Sourasish Basu <89185962+SourasishBasu@users.noreply.github.com>
2023-10-27 16:26:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,89 +1,44 @@
-from tkinter import *
-import math
-# ---------------------------- CONSTANTS ------------------------------- #
-PINK = "#e2979c"
-RED = "#e7305b"
-GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
-FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
-reps = 0
-timer = None
+import requests
+import os
+from twilio.rest import Client
 
-# ---------------------------- TIMER MECHANISM ------------------------------- #
+# Find your Account SID and Auth Token at twilio.com/console and set the environment variables in your local environment.
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
 
-def timer_start():
-    global reps
-    reps += 1
+weather_codes = []
+flag = 0
 
-    work_sec = WORK_MIN * 60
-    short_break_sec = SHORT_BREAK_MIN * 60
-    long_break_sec = LONG_BREAK_MIN * 60
+parameters = {
+    "q": "City,Country",  # Example: "London,UK"
+    "days": 1,
+    "alerts": "no",
+    "aqi": "no",
+    "key": os.environ["WEATHER_API_KEY"]
+}
 
-    if reps % 8 == 0:
-        count_down(long_break_sec)
-        timer_label.config(text = "Break", fg = RED)
-    elif reps % 2 == 0:
-        count_down(short_break_sec)
-        timer_label.config(text = "Break", fg = PINK)
-    else:
-        count_down(work_sec)
-        timer_label.config(text = "Work", fg = RED)
+url = "http://api.weatherapi.com/v1/forecast.json" 
 
+# Receive hourly weather forecast info for my location
+response = requests.get(url, params=parameters)
+data = response.json()
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
-def count_down(count):
-    min = math.floor(count/60)
-    sec = count % 60
-    if sec < 10:
-        sec = f"0{sec}"
+# Parsing the json to extract the weather codes of next 12 hours from the data
+for i in range(0, 12):
+    weather_codes.append(data["forecast"]["forecastday"][0]["hour"][i]["condition"]["code"])
 
-    canvas.itemconfig(timer_text, text=f"{min}:{sec}")
-    if count > 0:
-        global timer
-        timer = window.after(1000, count_down, count-1)
-    else:
-        timer_start()
-        mark=""
-        work_sessions = math.floor(reps/2)
-        for i in range(work_sessions):
-            mark += "✓"
-        check.config(text = mark)
+# Checking codes to find whether possibility of rain in 12 hours
+for j in range(len(weather_codes)):
+    if weather_codes[j] > 1030 and weather_codes[j] not in [1135, 1147]:
+        flag = 1
+        break
 
-
-# ---------------------------- TIMER RESET ------------------------------- #
-def timer_reset():
-    window.after_cancel(timer_text)
-    canvas.itemconfig(timer_text, text = "00:00")
-    timer_label.config(text = "Timer")
-    check.config(text = "")
-    global reps
-    reps = 0
-
-# ---------------------------- UI SETUP ------------------------------- #
-window = Tk()
-window.title("Pomodoro")
-window.config(padx=100,pady=50, bg=YELLOW)
-
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_img = PhotoImage(file="assets/tomato.png")
-canvas.create_image(100,112, image=tomato_img)
-timer_text = canvas.create_text(100,130,text="00:00", fill="white",font=("Arial",30,"bold"))
-canvas.grid(column=1, row=1)
-
-timer_label = Label(text="TIMER", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
-timer_label.grid(column=1, row=0)
-
-start = Button(text="Start", highlightthickness=0, command = timer_start)
-start.grid(column=0, row=2)
-
-reset = Button(text="Reset", highlightthickness=0, command=timer_reset)
-reset.grid(column=2, row=2)
-
-check=Label(fg=GREEN, bg =YELLOW, font=("Arial", 20, "bold"))
-check.grid(column=1, row=3)
-
-
-window.mainloop()
+# Sending SMS via Twilio API if it rains
+if flag == 1:
+    msg = "Its going to rain today. Remember to bring your umbrella!"
+    message = client.messages \
+        .create(
+        body=f"{msg}",
+        from_='TWILIO_PHONE_NUMBER',
+        to='YOUR_VERIFIED_PHONE_NUMBER')
